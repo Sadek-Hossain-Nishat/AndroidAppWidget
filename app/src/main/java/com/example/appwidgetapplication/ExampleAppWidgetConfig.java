82fc73017e54c1890d4ee839e2c7bd1cbe355dfa
Sadek Hossain Nishat <65479688+Sadek-Hossain-Nishat@users.noreply.github.com>
2023-01-23 07:03:32 -0800

    sixth commit

--- app/src/main/java/com/example/appwidgetapplication/ExampleAppWidgetConfig.java
+++ app/src/main/java/com/example/appwidgetapplication/ExampleAppWidgetConfig.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RemoteViews;
 
 public class ExampleAppWidgetConfig extends AppCompatActivity {
 
@@ -31,6 +34,10 @@ public class ExampleAppWidgetConfig extends AppCompatActivity {
                     AppWidgetManager.INVALID_APPWIDGET_ID);
         }
 
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,appWidgetId);
+        setResult(RESULT_CANCELED,resultValue);
+
         if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
 
             finish();
@@ -42,9 +49,25 @@ public class ExampleAppWidgetConfig extends AppCompatActivity {
 
     public void confirmConfiguration(View view) {
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-    Intent intent = new Intent(this,MainActivity.class);
-    PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
 
+        String buttonText = editTextButton.getText().toString();
 
+        RemoteViews views = new RemoteViews(this.getPackageName(),R.layout.example_widget);
+        views.setOnClickPendingIntent(R.id.id_example_widget_button,pendingIntent);
+        views.setCharSequence(R.id.id_example_widget_button,"setText",buttonText);
+        views.setInt(R.id.id_example_widget_button,"setBackgroundColor",
+                Color.RED);
+        views.setBoolean(R.id.id_example_widget_button,"setEnabled",false);
+        appWidgetManager.updateAppWidget(appWidgetId,views);
+        SharedPreferences prefs = getSharedPreferences(SHARED_PRESS,MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(KEY_BUTTON_TEXT+appWidgetId,buttonText);
+        editor.apply();
+        Intent resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,appWidgetId);
+        setResult(RESULT_OK,resultValue);
+        finish();
     }
 }
